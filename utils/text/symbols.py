15ce46bb82546d6d6c1fef97ac2734ed428abb5f
Kamil-Kisielewicz <kamiljkisielewicz@gmail.com>
2020-06-29 08:52:45 -0700

    Update symbols.py

--- utils/text/symbols.py
+++ utils/text/symbols.py
@@ -9,7 +9,7 @@ from utils.text import cmudict
 
 _pad = '_'
 _eos = '&'
-_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÇÃÀÁÂÊÉÍÓÔÕÚÛabcdefghijklmnopqrstuvwxyzçãàáâêéíóôõúû!(),-.:;? '
+_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜẞabcdefghijklmnopqrstuvwxyzäöüß!(),-.:;? '
 _punctuations = '!(),-.:;? '
 _phoneme_punctuations = '.!;:,? '
 
