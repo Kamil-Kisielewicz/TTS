3e75f6df2411aa43ee3e1577d7034f56d669e646
Kamil-Kisielewicz <kamiljkisielewicz@gmail.com>
2020-06-30 11:54:46 -0700

    Update symbols.py

--- utils/text/symbols.py
+++ utils/text/symbols.py
@@ -20,6 +20,7 @@ def make_symbols(characters, phonemes, punctuations='!\'(),-.:;? ', pad='_', eos
 
 _pad = '_'
 _eos = '&'
+_bos = '^'
 _characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜẞabcdefghijklmnopqrstuvwxyzäöüß!(),-.:;? '
 _punctuations = '!(),-.:;? '
 _phoneme_punctuations = '.!;:,? '
