ee768ee6f8d34f812fe556d622e903b530e43e9f
Kamil-Kisielewicz <kamiljkisielewicz@gmail.com>
2020-08-19 11:04:41 -0700

    Update losses.py

--- layers/losses.py
+++ layers/losses.py
@@ -157,8 +157,8 @@ class GuidedAttentionLoss(torch.nn.Module):
 
     @staticmethod
     def _make_ga_mask(ilen, olen, sigma):
-        grid_x, grid_y = torch.meshgrid(torch.arange(olen), torch.arange(ilen))
-        grid_x, grid_y = grid_x.float(), grid_y.float()
+        grid_x, grid_y = torch.meshgrid(torch.arange(olen), torch.arange(ilen)).to(self.device)
+        grid_x, grid_y = grid_x.float().to(self.device), grid_y.float().to(self.device)
         return 1.0 - torch.exp(-(grid_y / ilen - grid_x / olen) ** 2 / (2 * (sigma ** 2)))
 
     @staticmethod
