d966276e9e2b6887b061f986765cb23f214b7bea
Kamil-Kisielewicz <kamiljkisielewicz@gmail.com>
2020-08-19 09:36:30 -0700

    Update TTSDataset.py

--- datasets/TTSDataset.py
+++ datasets/TTSDataset.py
@@ -70,7 +70,7 @@ class MyDataset(Dataset):
         self.sort_items()
 
     def load_wav(self, filename):
-        audio = self.ap.load_wav(filename, sr=self.ap.sample_rate)
+        audio = self.ap.load_wav(filename)#, sr=self.ap.sample_rate)
         return audio
 
     @staticmethod
