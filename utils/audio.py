09b9216290bc109600e35c4464d8e3e93b9598a7
Kamil-Kisielewicz <kamiljkisielewicz@gmail.com>
2020-08-19 09:40:04 -0700

    Update audio.py

--- utils/audio.py
+++ utils/audio.py
@@ -304,8 +304,8 @@ class AudioProcessor(object):
 
     ### save and load ###
     def load_wav(self, filename, sr=None):
-        if '.mp3' not in filename:
-            return None
+#         if '.mp3' not in filename:
+#             return None
         if sr is None:
             x, sr = sf.read(filename)
         else:
